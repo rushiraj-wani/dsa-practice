c986ebffb2eedd78f5251bb5e03d361dfd4b12f0
Rushiraj Wani <rajwani2003@gmail.com>
2025-06-16 15:06:05 +0530

    Min Max element in array using min-max inbuilt funtion

---
+++ arrays/5.2_Min-Max2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+void minMaxEle(vector<int>& vec){
+    int n = vec.size();
+    int minEle = INT_MAX; // Largest num is min.
+    int maxEle = INT_MIN; // Smallest num is max.
+
+    for(int i=0; i<n; i++){
+        minEle = min(minEle, vec[i]);
+        maxEle = max(maxEle, vec[i]);
+    }
+    cout << "Min ele: " << minEle << ", Max ele: " << maxEle << "\n";
+}
+
+int main(){
+    vector<int> vec = {3,6,82,1,9,13};
+
+    minMaxEle(vec);
+
+    return 0;
+}
